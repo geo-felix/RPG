9fcf27ff328b8ebb0d0ff7bc0370bb5bb5da60f7
its-kfelix <felixodhiambo538@gmail.com>
2022-09-02 10:50:24 +0300

    second commit

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RPG.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController:ControllerBase
+    {
+        private static Character knight = new Character();
+         public IActionResult Get(){
+            return Ok(knight);
+         }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+global using RPG.Model;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
